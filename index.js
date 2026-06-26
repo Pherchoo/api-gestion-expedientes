9b30078de06533242f506bcb25662d44fc965e6f
Christopher Luna <alchris4828@gmail.com>
2026-06-25 19:30:27 -0600

    Base configuration

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-﻿// Punto de entrada - API de Gestion de Expedientes
+﻿require('dotenv').config();
+const express = require('express');
+const helmet = require('helmet');
+
+const app = express();
+app.use(helmet());
+
+const PORT = process.env.PORT || 5100;
+
+app.listen(PORT, () => {
+  console.log('Hello World');
+});
